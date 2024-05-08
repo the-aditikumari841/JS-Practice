10b55629b6ad8018fd65d4cce1e39d56235add35
Aditi Kumari <aditikumari8421@gmail.com>
2024-05-08 20:58:35 +0530

    commit

---
+++ Basic-JS-APIs/String-Functions/stringFindIndex.js
@@ -0,0 +1,17 @@
+function findIndexofString(str1, str2) {
+    console.log("",);
+    console.log("Index of string '",str2, "' in ", "the string '", str1 , "' is :" , str1.indexOf(str2) );
+}
+
+findIndexofString("Hello World", "World");
+findIndexofString("Hello World World", "World");
+findIndexofString("Hello World", "Hello");
+findIndexofString("Hello World", "dhfkwWorld");
+
+function findLastIndexofString(str1, str2) {
+    console.log("",);
+    console.log("Index of last string '",str2, "' in ", "the string '", str1 , "' is :" , str1.lastIndexOf(str2) );
+}
+
+findLastIndexofString("Hello World World", "World");
+findLastIndexofString("Hello World World World", "World");
---
+++ Basic-JS-APIs/String-Functions/stringLength.js
@@ -0,0 +1,6 @@
+function getLength(str) {
+    console.log("Original String :", str);
+    console.log("Length of string :", str.length);
+}
+
+getLength("Hello World");
