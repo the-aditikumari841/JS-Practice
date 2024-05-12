557d7547f3e4dc23ec34159addc0c0c6f6d4ec0b
Aditi Kumari <aditikumari8421@gmail.com>
2024-05-12 17:35:09 +0530

    commit

---
+++ Basic-JS-APIs/String-Functions/arrayPushPopShiftUnshift.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);   //[ 1, 2, 3, 4, 5, 6 ]
+arr.pop();
+console.log(arr);   //[ 1, 2, 3, 4, 5 ]
+arr.push(2);
+console.log(arr);   //[ 1, 2, 3, 4, 5, 2 ]
+arr.shift(9);
+console.log(arr);   //[ 2, 3, 4, 5, 2 ]
+arr.unshift(1);
+console.log(arr);   //[ 1, 2, 3, 4, 5, 2 ]
